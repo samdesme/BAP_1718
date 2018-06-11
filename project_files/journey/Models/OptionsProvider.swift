830d6b2a8d671da353490d04b31620f7fae5ef64
samdesme <sam.desmedt@student.arteveldehs.be>
2018-06-11 19:29:55 +0200

    finished chart 2

--- project_files/journey/Models/OptionsProvider.swift
+++ project_files/journey/Models/OptionsProvider.swift
@@ -11,31 +11,30 @@ import Highcharts
 
 class OptionsProvider {
     
-    class func provideOptions(forChartType options: String, series: [Any], type: String, xValues: [String]) -> HIOptions {
+    class func provideOptions(forChartType options: String, series: [Any], xValues: [String]) -> HIOptions {
         var categories = [String]()
         var step: NSNumber?
-        
-        if type == "day" {
+        var values = [Any]()
+    
             categories = xValues
             step = 1
+        values = series
+    
+        if(series.count == 0){
+            values.append(0)
+            categories.append("No data yet")
         }
         
-        if type == "month" {
-            categories = xValues
-            step = 1
-        }
+        print("options: \(String(describing: options))")
         
-        if type == "year" {
-            categories = xValues
-            step = 1
-        }
+
         
         
         if options == "column" {
             let hioptions = HIOptions()
             
             let chart = HIChart()
-            chart.backgroundColor = HIColor(linearGradient: ["x1": 0, "y1": 0, "x2": 0, "y2": 1], stops: [[0, "rgb(66, 218, 113)"], [1, "rgb(80, 140, 200)"]])
+            chart.backgroundColor = HIColor(linearGradient: ["x1": 0, "y1": 0, "x2": 0, "y2": 1], stops: [[0, "rgba(255, 255, 255, 0)"], [1, "rgba(202, 159, 201, 0)"]])
             chart.borderRadius = 6
             chart.type = options
             hioptions.chart = chart
@@ -53,7 +52,7 @@ class OptionsProvider {
             
             let plotOptions = HIPlotOptions()
             plotOptions.column = HIColumn()
-            plotOptions.column.color = HIColor(rgba: 255, green: 255, blue: 255, alpha: 0.6)
+            plotOptions.column.color = HIColor(rgba: 202, green: 159, blue: 201, alpha: 1)
             plotOptions.column.borderRadius = 2
             plotOptions.column.borderWidth = 0
             hioptions.plotOptions = plotOptions
@@ -63,24 +62,16 @@ class OptionsProvider {
             hioptions.credits = credits
             
             let title = HITitle()
-            title.text = "Graph Column"
+            title.text = ""
             title.align = "left"
             title.style = HIStyle()
             title.style.fontFamily = "Arial"
             title.style.fontSize = "14px"
-            title.style.color = "rgba(255, 255, 255, 0.6)"
+            title.style.color = "rgba(202, 159, 201, 0.6)"
             title.y = 16
             hioptions.title = title
             
-            let subtitle = HISubtitle()
-            subtitle.text = "Subtitle"
-            if subtitle.text.characters.count > 0 {
-                subtitle.text = subtitle.text + " total"
-            }
-            subtitle.align = "left"
-            subtitle.style = ["fontFamily": "Arial", "fontSize": "10px", "color": "rgba(255, 255, 255, 0.6)"]
-            subtitle.y = 28
-            hioptions.subtitle = subtitle
+
             
             let tooltip = HITooltip()
             tooltip.headerFormat = ""
@@ -88,10 +79,10 @@ class OptionsProvider {
             
             let xaxis = HIXAxis()
             xaxis.tickColor = HIColor(rgba: 255, green: 255, blue: 255, alpha: 0.0)
-            xaxis.lineColor = HIColor(rgba: 255, green: 255, blue: 255, alpha: 0.3)
+            xaxis.lineColor = HIColor(rgba: 202, green: 159, blue: 201, alpha: 1)
             xaxis.labels = HILabels()
             xaxis.labels.style = HIStyle()
-            xaxis.labels.style.color = "rgb(255, 255, 255)"
+            xaxis.labels.style.color = "rgba(202, 159, 201, 0)"
             xaxis.labels.style.textOutline = "10px Arial"
             xaxis.labels.step = step
             xaxis.categories = categories
@@ -100,10 +91,11 @@ class OptionsProvider {
             let yaxis = HIYAxis()
             yaxis.lineWidth = 1
             yaxis.gridLineWidth = 0
-            yaxis.lineColor = HIColor(rgba: 255, green: 255, blue: 255, alpha: 0.3)
+            yaxis.allowDecimals = false
+            yaxis.lineColor = HIColor(rgba: 202, green: 159, blue: 201, alpha: 1)
             yaxis.labels = HILabels()
             yaxis.labels.style = HIStyle()
-            yaxis.labels.style.color = "rgb(255, 255, 255)"
+            yaxis.labels.style.color = "rgb(202, 159, 201)"
             yaxis.labels.style.textOutline = "10px Arial"
             yaxis.labels.x = -5
             yaxis.title = HITitle()
@@ -114,10 +106,10 @@ class OptionsProvider {
             let column = HIColumn()
             column.tooltip = HITooltip()
             column.tooltip.headerFormat = ""
-            column.tooltip.valueSuffix = " kcal"
+            column.tooltip.valueSuffix = ""
             column.showInLegend = false
             column.data = series
-            column.name = "Column name"
+            column.name = "Amount"
             hioptions.series = [column]
             
             return hioptions
@@ -167,18 +159,6 @@ class OptionsProvider {
             title.y = 16
             hioptions.title = title
             
-            /*
-            let subtitle = HISubtitle()
-            subtitle.text = "Subtitle"
-            if subtitle.text.characters.count > 0 {
-                subtitle.text = subtitle.text + " total"
-            }
-            subtitle.align = "left"
-            subtitle.style = ["fontFamily": "Arial", "fontSize": "10px", "color": "rgba(255, 255, 255, 0.6)"]
-            subtitle.y = 28
-            hioptions.subtitle = subtitle
-            */
-            
             let tooltip = HITooltip()
             tooltip.headerFormat = ""
             hioptions.tooltip = tooltip
@@ -216,7 +196,7 @@ class OptionsProvider {
             spline.tooltip.headerFormat = ""
             spline.tooltip.valueSuffix = ""
             spline.showInLegend = false
-            spline.data = series
+            spline.data = values
             spline.name = "Mood"
             hioptions.series = [spline]
             
--- project_files/journey/ViewControllers/Stats/MoodViewController.swift
+++ project_files/journey/ViewControllers/Stats/MoodViewController.swift
@@ -19,33 +19,39 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
     var scrollView = UIScrollView()
     var viewTopGradient = UIView()
     var viewGraph1 = UIView()
+    var viewGraph2 = UIView()
+
 
     let  topGradientLayer = CAGradientLayer()
     let btnGradientLayer = CAGradientLayer()
     let  gradientLayer = CAGradientLayer()
     
     var configuration: [String: Any]!
-
     var chartType: String!
-    
     var data: [String : Any]!
-    
     var arrCategories = [String]()
+    var arrMoods = ["1","2","3","4","5"]
+    
+    var arrCount = Array(repeating: 0, count: 5)
 
-
+    var day = Array(repeating: 0, count: 24)
+    var month = Array(repeating: 0, count: 31)
+    var year = Array(repeating: 0, count: 12)
   
-    var chartView: HIChartView!
+    var chartView1: HIChartView!
+    var chartView2: HIChartView!
+
 
     let toolbar = Bundle.main.loadNibNamed("toolbarView", owner: nil, options: nil)?.first as! toolbarView
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
     
-    var day = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+   /* var day = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     var month = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     var year = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-
+     */
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -92,13 +98,14 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
         topGradientLayer.locations = [ 0.0, 1.0]
         
         setUpGraphView1()
+        setUpGraphView2()
    
         
     }
     
     func setUpGraphView1() {
         
-        viewGraph1.frame = CGRect(x: 15, y: 15, width: self.view.frame.size.width - 30, height: (scrollView.frame.size.height/2))
+        viewGraph1.frame = CGRect(x: 15, y: 15, width: self.view.frame.size.width - 30, height: (self.view.frame.size.height  - (tabBarController?.tabBar.frame.size.height)!*2)/5*3 - 60)
         viewGraph1.backgroundColor = whiteColor
         
         // edit corner radius of the view
@@ -128,21 +135,40 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
         
     }
     
+    func setUpGraphView2() {
+        
+        viewGraph2.frame = CGRect(x: 15, y: viewGraph1.frame.size.height + 30, width: self.view.frame.size.width - 30, height: (self.view.frame.size.height  - (tabBarController?.tabBar.frame.size.height)!*2)/5*2 - 60)
+        viewGraph2.backgroundColor = whiteColor
+        
+        // edit corner radius of the view
+        viewGraph2.layer.cornerRadius = 25
+        viewGraph2.layer.shadowColor = blackColor.cgColor
+        viewGraph2.layer.shadowOffset = CGSize(width: 6, height: 6)
+        viewGraph2.layer.shadowOpacity = 0.05
+        //viewGraph1.layer.shadowRadius = 5.0
+        viewGraph2.layer.shadowRadius = 10.0
+        
+        scrollView.addSubview(viewGraph2)
+        
+        setUpGraph2()
+        
+    }
+    
     func setUpGraph1() {
         
-        let lblGraph1 = UILabel()
+        let lblGraph = UILabel()
         
         // Label
         
-        lblGraph1.text = "Evolution of your mood on time"
-        lblGraph1.frame = CGRect(x: 0, y: 15, width: viewGraph1.frame.size.width, height: 25)
-        lblGraph1.textColor = whiteColor
-        lblGraph1.textAlignment = .center
-        lblGraph1.font = fontLabel
+        lblGraph.text = "Evolution of your mood on time"
+        lblGraph.frame = CGRect(x: 0, y: 15, width: viewGraph1.frame.size.width, height: 25)
+        lblGraph.textColor = whiteColor
+        lblGraph.textAlignment = .center
+        lblGraph.font = fontLabel
         
         // Toolbar
         
-        toolbar.frame = CGRect(x: 0, y: lblGraph1.frame.size.height + 30, width: viewGraph1.frame.size.width, height: 44)
+        toolbar.frame = CGRect(x: 0, y: lblGraph.frame.size.height + 30, width: viewGraph1.frame.size.width, height: 44)
         toolbar.backgroundColor = UIColor.clear
 
         toolbar.segment.tintColor = whiteColor
@@ -157,18 +183,20 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
         
         // Chartview layout
         
-        self.chartView = HIChartView(frame: viewGraph1.bounds)
-        getData(period: "day")
-        chartView.backgroundColor = UIColor.clear
+   self.chartView1 = HIChartView(frame: CGRect(x: 15 + 30, y: lblGraph.frame.size.height + 15 + toolbar.frame.size.height + 30, width: viewGraph1.frame.size.width - 30 - 30, height: viewGraph1.frame.size.height - lblGraph.frame.size.height - toolbar.frame.size.height - 30*2))
         
-        self.chartView = HIChartView(frame: CGRect(x: 15 + 30, y: lblGraph1.frame.size.height + 15 + toolbar.frame.size.height + 30, width: viewGraph1.frame.size.width - 30 - 30, height: viewGraph1.frame.size.height - lblGraph1.frame.size.height - toolbar.frame.size.height - 30*2))
-        self.chartView.delegate = self
+        getDataTime(period: "day")
         
-        self.chartView.viewController = self
+        chartView1.backgroundColor = UIColor.clear
+        
+     
+        self.chartView1.delegate = self
+        
+        self.chartView1.viewController = self
         
         
-        viewGraph1.addSubview(lblGraph1)
-        viewGraph1.addSubview(self.chartView!)
+        viewGraph1.addSubview(lblGraph)
+        viewGraph1.addSubview(self.chartView1!)
         viewGraph1.addSubview(toolbar)
         
         let imgView1 = UIImageView()
@@ -176,10 +204,11 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
         let imgView3 = UIImageView()
         let imgView4 = UIImageView()
         let imgView5 = UIImageView()
-        let yView = UIView()
-        let scale = self.chartView.frame.size.height/5
         
-        yView.frame = CGRect(x: 0, y: lblGraph1.frame.size.height + 15 + toolbar.frame.size.height + 30, width: 30, height: chartView.frame.size.height)
+        let sideView = UIView()
+        let scale = self.chartView1.frame.size.height/5
+        
+        sideView.frame = CGRect(x: 0, y: lblGraph.frame.size.height + 15 + toolbar.frame.size.height + 30, width: 30, height: chartView1.frame.size.height)
         
         
         imgView1.image = UIImage(named: "ic_mood5_white")
@@ -195,21 +224,83 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
         imgView5.frame = CGRect(x: 15, y: scale*4, width: 30, height: 30)
         
         
-        yView.addSubview(imgView1)
-        yView.addSubview(imgView2)
-        yView.addSubview(imgView3)
-        yView.addSubview(imgView4)
-        yView.addSubview(imgView5)
+        sideView.addSubview(imgView1)
+        sideView.addSubview(imgView2)
+        sideView.addSubview(imgView3)
+        sideView.addSubview(imgView4)
+        sideView.addSubview(imgView5)
         
-        viewGraph1.addSubview(yView)
+        viewGraph1.addSubview(sideView)
         
         
     }
     
+    func setUpGraph2() {
+        
+        let lblGraph = UILabel()
+        
+        // Label
+        lblGraph.text = "Mood count"
+        lblGraph.frame = CGRect(x: 0, y: 15, width: viewGraph2.frame.size.width, height: 25)
+        lblGraph.textColor = blackColor
+        lblGraph.textAlignment = .center
+        lblGraph.font = fontLabel
+    
+        
+        // Chartview layout
+        self.chartView2 = HIChartView(frame: CGRect(x: 15, y: lblGraph.frame.size.height, width: viewGraph2.frame.size.width - 30, height: viewGraph2.frame.size.height - lblGraph.frame.size.height - 15))
+        
+        getDataCount()
+        chartView2.backgroundColor = UIColor.clear
+        
+    
+        self.chartView2.delegate = self
+        self.chartView2.viewController = self
+        
+        viewGraph2.addSubview(lblGraph)
+        viewGraph2.addSubview(self.chartView2!)
+        
+        let imgView1 = UIImageView()
+        let imgView2 = UIImageView()
+        let imgView3 = UIImageView()
+        let imgView4 = UIImageView()
+        let imgView5 = UIImageView()
+        let bottomView = UIView()
+        
+        let scale = (self.chartView2.frame.size.width - 35)/5
+        let height = lblGraph.frame.size.height + self.chartView2.frame.size.height - 25
+        
+        bottomView.frame = CGRect(x: 55, y: height, width: self.chartView2.frame.size.width, height: 30)
+        
+        bottomView.backgroundColor = UIColor.clear
+        
+        imgView1.image = UIImage(named: "ic_mood5_outline")
+        imgView2.image = UIImage(named: "ic_mood4_outline")
+        imgView3.image = UIImage(named: "ic_mood3_outline")
+        imgView4.image = UIImage(named: "ic_mood2_outline")
+        imgView5.image = UIImage(named: "ic_mood1_outline")
+        
+        imgView1.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        imgView2.frame = CGRect(x: scale, y: 0, width: 30, height: 30)
+        imgView3.frame = CGRect(x: scale*2, y: 0, width: 30, height: 30)
+        imgView4.frame = CGRect(x: scale*3, y: 0, width: 30, height: 30)
+        imgView5.frame = CGRect(x: scale*4, y: 0, width: 30, height: 30)
+        
+        
+        bottomView.addSubview(imgView1)
+        bottomView.addSubview(imgView2)
+        bottomView.addSubview(imgView3)
+        bottomView.addSubview(imgView4)
+        bottomView.addSubview(imgView5)
+        
+        viewGraph2.addSubview(bottomView)
+        
+        
+    }
     
     //MARK: - Data functions
     
-    func getData(period: String) {
+    func getDataTime(period: String) {
         
         arrCategories.removeAll()
         
@@ -289,9 +380,7 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
             let entryPeriod = formatterPeriod.string(from: entry.date)
             let entryScale = formatterScale.string(from: entry.date)
             
-            print("entryScale: \(String(describing: entryScale))")
-            print("strScale: \(String(describing: strScale))")
-            print("-----")
+            
 
             if(entryScale == strScale){
                 
@@ -351,11 +440,45 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
            
         }
         
-        self.chartView.options = OptionsProvider.provideOptions(forChartType: "spline", series: arrayData.filter {$0 != 0}, type: "\(period)", xValues: arrCategories)
+        //print("arrCategories: \(String(describing: arrCategories))")
+        //print("arrayData: \(String(describing: arrayData))")
         
-       
+        self.chartView1.options = OptionsProvider.provideOptions(forChartType: "spline", series: arrayData.filter {$0 != 0}, xValues: arrCategories)
+        
 
+    }
+    
+    func getDataCount() {
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequestEntries = NSFetchRequest<Entries>(entityName: "Entries")
+        
+        
+        let allEntries = try! context.fetch(fetchRequestEntries)
+        
+        for entry in allEntries {
+            
+            switch entry.mood {
+            case 1:
+                arrCount[0] += 1
+            case 2:
+                arrCount[1] += 1
+            case 3:
+                arrCount[2] += 1
+            case 4:
+                arrCount[3] += 1
+            default:
+                arrCount[4] += 1
+            
+            }
+    
 
+            
+     self.chartView2.options = OptionsProvider.provideOptions(forChartType: "column", series: arrCount, xValues: arrMoods)
+            
+            
+        }
+        
     }
     
     //MARK: - Actions
@@ -366,11 +489,11 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
         switch sender.selectedSegmentIndex {
         case 0:
     
-            getData(period: "day")
+            getDataTime(period: "day")
         case 1:
-            getData(period: "month")
+            getDataTime(period: "month")
         case 2:
-           getData(period: "year")
+           getDataTime(period: "year")
         default:
             break
         }
@@ -386,7 +509,7 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
         case 0:
             dataName = "day"
             arrayData = day
-            getData(period: "day")
+            getDataTime(period: "day")
         case 1:
             dataName = "month"
             arrayData = month
@@ -405,7 +528,7 @@ class MoodViewController: UIViewController, HIChartViewDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         
-        getData(period: "day")
+        getDataTime(period: "day")
     }
 
     //MARK: - HIChartViewDelegate
